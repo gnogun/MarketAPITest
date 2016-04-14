15376adcc5320fd69d3e0ce4e3a9345678bba996
Wilson <wilso.song@latisglobal.com>
2016-04-14 10:36:57 +0900

    다시 커밋

--- MarketAPITest/src/test/java/test/AmazonAddTest.java
+++ MarketAPITest/src/test/java/test/AmazonAddTest.java
@@ -2,26 +2,11 @@ package test;
 
 import org.junit.Test;
 
-import com.amazonservices.mws.sellers.MarketplaceWebServiceSellersClient;
-import com.amazonservices.mws.sellers.model.GetServiceStatusRequest;
-import com.amazonservices.mws.sellers.samples.GetServiceStatusSample;
-import com.amazonservices.mws.sellers.samples.MarketplaceWebServiceSellersSampleConfig;
-
 public class AmazonAddTest {
 
 	@Test
 	public void add() {
-		MarketplaceWebServiceSellersClient client = MarketplaceWebServiceSellersSampleConfig.getClient();
-
-		// Create a request.
-		GetServiceStatusRequest request = new GetServiceStatusRequest();
-		String sellerId = "example";
-		request.setSellerId(sellerId);
-		String mwsAuthToken = "example";
-		request.setMWSAuthToken(mwsAuthToken);
-
-		// Make the call.
-		GetServiceStatusSample.invokeGetServiceStatus(client, request);
+		
 	}
 
 }
